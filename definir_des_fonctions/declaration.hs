1e4b7b3b075270d9db93a2c8e99c67c45995d7ed
Stephane <stephane@lopes-neves.fr>
2016-12-30 22:37:39 +0100

    My min/max with 4 params

--- definir_des_fonctions/declaration.hs
+++ definir_des_fonctions/declaration.hs
@@ -1,7 +1,8 @@
 myMin (x, y)
     | x > y = y
     | otherwise = x
-	
+myMinFor4 (w,x,y,z) = myMin(myMin(w,x), myMin(y,z))
 myMax (x, y)
     | x < y = y
-	| otherwise = x
+    | otherwise = x
+myMaxFor4 (w,x,y,z) = myMax(myMax(w,x), myMax(y,z))
